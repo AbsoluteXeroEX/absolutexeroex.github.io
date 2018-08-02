6331d6bd3ee594d36f2ff3c187f5600ad74e082d
AbsoluteXeroEX <40545575+AbsoluteXeroEX@users.noreply.github.com>
2018-08-01 19:11:06 -0500

    Update widgets.js

--- static/js/widgets.js
+++ static/js/widgets.js
@@ -1,9 +1,18 @@
 var modals = document.querySelectorAll('.modal');
 var a = document.querySelectorAll("figcaption.modalBtn");
+var b = document.querySelectorAll("a.modalBtn");
 var spans = document.getElementsByClassName("close");
 
 for (var i = 0; i < a.length; i++) {
   a[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+  }
+}
+
+for (var i = 0; i < b.length; i++) {
+  b[i].onclick = function(e) {
     e.preventDefault();
     modal = document.querySelector(e.target.getAttribute("href"));
     modal.style.display = "block";
